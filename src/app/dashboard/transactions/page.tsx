786268173b276bdd37e6c702d9bb6c5a09a6721c
Vladimir Gulev <vladinson009@icloud.com>
2025-12-24 23:23:18 +0100

    Created: Transactions table

--- src/app/dashboard/transactions/page.tsx
+++ src/app/dashboard/transactions/page.tsx
@@ -8,10 +8,21 @@ import {
 } from '@/components/ui/breadcrumb';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
 import { getTransactionByMonth } from '@/data/getTransactionsByMonth';
 import { format } from 'date-fns';
+import { PencilIcon } from 'lucide-react';
 import Link from 'next/link';
 import z from 'zod';
+import numeral from 'numeral';
+import { Badge } from '@/components/ui/badge';
 
 type Props = {
   searchParams: Promise<{ year?: string; month?: string }>;
@@ -67,6 +78,62 @@ const TransactionsPage = async ({ searchParams }: Props) => {
           <Button asChild>
             <Link href="/dashboard/transactions/new">New Transaction</Link>
           </Button>
+          {!transactions?.length && (
+            <p className="text-center py-10 text-lg text-muted-foreground">
+              There are no transactions for this month
+            </p>
+          )}
+          {!!transactions?.length && (
+            <Table className="mt-4">
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Date</TableHead>
+                  <TableHead>Description</TableHead>
+                  <TableHead>Type</TableHead>
+                  <TableHead>Category</TableHead>
+                  <TableHead>Amount</TableHead>
+                  <TableHead />
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {transactions.map((transaction) => (
+                  <TableRow key={transaction.id}>
+                    <TableCell>
+                      {format(transaction.transactionDate, 'do MMM yyyy')}
+                    </TableCell>
+                    <TableCell>{transaction.description}</TableCell>
+                    <TableCell className="capitalize">
+                      <Badge
+                        className={
+                          transaction.transactionType === 'income'
+                            ? 'bg-lime-500'
+                            : 'bg-orange-500'
+                        }
+                      >
+                        {transaction.transactionType}
+                      </Badge>
+                    </TableCell>
+                    <TableCell>{transaction.category}</TableCell>
+                    <TableCell>
+                      € {numeral(transaction.amount).format('0,0[.]00')}
+                    </TableCell>
+                    <TableCell className="text-right">
+                      <Button
+                        variant="outline"
+                        asChild
+                        size="icon"
+                        aria-label="Edit transaction"
+                      >
+                        <Link href={`/dashboard/transactions/${transaction.id}`}>
+                          <PencilIcon />
+                        </Link>
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          )}
         </CardContent>
       </Card>
     </div>
