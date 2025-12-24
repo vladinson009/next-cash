1a6711307eecfa54dea508e2f87ff329c4933127
Vladimir Gulev <vladinson009@icloud.com>
2025-12-24 21:03:33 +0100

    added: Toast component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,7 @@ import {
 } from '@clerk/nextjs';
 import { Button } from '@/components/ui/button';
 import UserDropdown from '@/components/navbar/user-dropdown';
+import { Toaster } from '@/components/ui/sonner';
 
 const poppins = Poppins({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
@@ -54,6 +55,7 @@ export default function RootLayout({
             </div>
           </nav>
           {children}
+          <Toaster />
         </body>
       </html>
     </ClerkProvider>
