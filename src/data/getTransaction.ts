ad2570b8da0c225262b62337270b52cbc7f7b294
Vladimir Gulev <vladinson009@icloud.com>
2025-12-25 17:05:03 +0100

    DB: Get transaction query

---
+++ src/data/getTransaction.ts
@@ -0,0 +1,25 @@
+import { db } from '@/db';
+import { transactionsTable } from '@/db/schema';
+import { auth } from '@clerk/nextjs/server';
+import { and, eq } from 'drizzle-orm';
+import 'server-only';
+
+export const getTransaction = async (transactionId: number) => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return null;
+  }
+
+  const [transaction] = await db
+    .select()
+    .from(transactionsTable)
+    .where(
+      and(
+        eq(transactionsTable.id, transactionId),
+        eq(transactionsTable.userId, userId)
+      )
+    );
+
+  return transaction;
+};
