d373d601e22085c3ebb44235d327b6ee0352421d
Vladimir Gulev <83925378+vladinson009@users.noreply.github.com>
2025-12-26 00:58:03 +0100

    Added: Custom error page for accessing invalid transaction id (#5)

---
+++ src/app/dashboard/transactions/[transactionId]/layout.tsx
@@ -0,0 +1,39 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb';
+import Link from 'next/link';
+
+const LayoutPage = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="max-w-7xl mx-auto py-10">
+      {/* Breadcrumb navigation */}
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href="/dashboard">Dashboard</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href="/dashboard/transactions">Transactions</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Edit Transaction</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      {children}
+    </div>
+  );
+};
+
+export default LayoutPage;
---
+++ src/app/dashboard/transactions/[transactionId]/not-found.tsx
@@ -0,0 +1,5 @@
+const NotFound = () => {
+  return <div className="text-3xl mt-5 ">Oops! Transaction not found.</div>;
+};
+
+export default NotFound;
