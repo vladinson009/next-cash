567bce291400a0c3669c3d9acc488b779e808d88
Vladimir Gulev <vladinson009@icloud.com>
2025-12-26 02:01:10 +0100

    Feature: Get recent transactions & Types

---
+++ src/data/getRecentTransactions.ts
@@ -0,0 +1,30 @@
+import { db } from '@/db';
+import { categoriesTable, transactionsTable } from '@/db/schema';
+import { auth } from '@clerk/nextjs/server';
+import { desc, eq } from 'drizzle-orm';
+import 'server-only';
+
+export const getRecentTransactions = async () => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return [];
+  }
+
+  const transactions = await db
+    .select({
+      id: transactionsTable.id,
+      description: transactionsTable.description,
+      amount: transactionsTable.amount,
+      transactionDate: transactionsTable.transactionDate,
+      category: categoriesTable.name,
+      transactionType: categoriesTable.type,
+    })
+    .from(transactionsTable)
+    .where(eq(transactionsTable.userId, userId))
+    .orderBy(desc(transactionsTable.transactionDate))
+    .limit(5)
+    .leftJoin(categoriesTable, eq(transactionsTable.categoryId, categoriesTable.id));
+
+  return transactions;
+};
