fadfd106dd1e272d387d3c95268848fea5fef2f2
Vladimir Gulev <vladinson009@icloud.com>
2026-01-01 20:47:52 +0100

    Fixed: Loading skeleton shape

--- src/app/dashboard/(dashboard-layout)/loading.tsx
+++ src/app/dashboard/(dashboard-layout)/loading.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from '@/components/ui/skeleton';
 const Loading = () => {
   return (
     <>
-      <Skeleton className="bg-zinc-200 w-full h-100 mb-5 " />
+      <Skeleton className="bg-zinc-200 w-full h-100 mb-5" />
       <Skeleton className="bg-zinc-200 w-full h-50 mb-4" />
     </>
   );
--- src/app/dashboard/transactions/[transactionId]/loading.tsx
+++ src/app/dashboard/transactions/[transactionId]/loading.tsx
@@ -1,7 +1,9 @@
 import { Skeleton } from '@/components/ui/skeleton';
 
 const LoadingState = () => {
-  return <Skeleton className="bg-zinc-200 w-full h-100 mt-4 max-w-3xl" />;
+  return (
+    <Skeleton className="bg-zinc-200 h-140 sm:h-100 max-w-3xl mx-1 md:mx-auto mt-4" />
+  );
 };
 
 export default LoadingState;
